c3c6041480f6e983c88d9d037d63db3ca4d6bb18
Gerardo Miranda <gerardodev25@gmail.com>
2023-04-12 22:45:01 -0400

    alreally i got it yeah apollo server config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 import { HellowordModule } from './helloword/hello-world.module';
 import { HelloworldResolver } from './helloword/hello-world.resolver';
@@ -11,7 +12,8 @@ import { HelloworldResolver } from './helloword/hello-world.resolver';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      // playground: false,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
     }),
     HellowordModule,
   ],
